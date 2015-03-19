3bd55fce64158fb2a6d50543f92dcd468419052e
birkin <birkin.diana@gmail.com>
2015-03-19 12:13:46 -0400

    starts.

---
+++ bdpy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+class BorrowDirect( object ):
+    """ Manages high-level function calls. """
+
+    def __init__( self ):
+        self.FOO = u'bar'
+
+    def say_hi( self ):
+        print u'hello_world'
