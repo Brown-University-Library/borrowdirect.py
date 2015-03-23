52f8d128f85de65fa4c0bcc20e4c7a2a740b074f
birkin <birkin.diana@gmail.com>
2015-03-23 10:42:26 -0400

    updates module name.

--- bdpy/__init__.py
+++ bdpy/__init__.py
@@ -1,1 +1,1 @@
-from .bdpy import BorrowDirect
+from .borrowdirect import BorrowDirect
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name='bdpy',
-    version='0.1-dev',
+    version='0.2-dev',
     packages=find_packages(),
 )
