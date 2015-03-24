5adedf6f589c72540281f869435dfd5e7060d74a
birkin <birkin.diana@gmail.com>
2015-03-24 14:43:14 -0400

    successfully grabs authentication-id.

--- bdpy/borrowdirect.py
+++ bdpy/borrowdirect.py
@@ -14,19 +14,19 @@ class BorrowDirect( object ):
             or a settings path to be passed in,
             or a dictionary to be passed in. """
         ## general initialization
-        self.API_AUTH_URL_ROOT = u'init'
-        self.UNIVERSITY_CODE = u'init'
-        self.LOG_PATH = u'init'
-        self.LOG_LEVEL = u'init'
-        self.logger = u'init'
+        self.API_AUTH_URL_ROOT = None
+        self.UNIVERSITY_CODE = None
+        self.LOG_PATH = None
+        self.LOG_LEVEL = None
+        self.logger = None
         ## setup
         bdh = BorrowDirectHelper()
         normalized_settings = bdh.normalize_settings( settings )
         bdh.update_properties( self, normalized_settings )
         bdh.setup_log( self )
         ## updated by workflow
-        self.authentication_id = None
-        self.authnz_is_valid = False
+        self.AId = None
+        self.authnz_is_valid = None
 
     def say_hi( self ):
         print u'API_AUTH_URL_ROOT, %s' % self.API_AUTH_URL_ROOT
@@ -36,15 +36,22 @@ class BorrowDirect( object ):
     def run_auth_nz( self, patron_barcode ):
         """ Runs authN/Z and stores authentication-id.
             Called manually. """
-        self.logger.info( u'starting...' )
-        self.logger.info( u'url, `%s`' % self.API_AUTH_URL_ROOT )
-        ## authn
+        self.logger.debug( u'starting...' )
+        self.logger.debug( u'url, `%s`' % self.API_AUTH_URL_ROOT )
+        ## authn - get the borrowdirect authentication-id
         d = {
             u'AuthenticationInformation': {
             u'LibrarySymbol': self.UNIVERSITY_CODE,
             u'PatronId': patron_barcode } }
+        self.logger.debug( u'params-dict, `%s`' % pprint.pformat(d) )
         headers = { u'Content-type': u'application/json', u'Accept': u'text/plain'}
-        # r = requests.post( self.API_AUTH_URL_ROOT, data=json.dumps(d), headers=headers )
+        r = requests.post( self.API_AUTH_URL_ROOT, data=json.dumps(d), headers=headers )
+        self.logger.debug( u'r.content, `%s`' % r.content.decode(u'utf-8') )
+        self.logger.debug( u'r.status_code, `%s`' % r.status_code )
+        dct = r.json()
+        self.logger.debug( u'response dct, `%s`' % pprint.pformat(dct) )
+        self.AId = dct[u'Authentication'][u'AuthnUserInfo'][u'AId']
+        self.logger.debug( u'self.AId, `%s`' % self.AId )
         return u'foo'
 
     # end class BorrowDirect
@@ -55,7 +62,7 @@ class BorrowDirectHelper( object ):
         Called by BorrowDirect.__init__() """
 
     def normalize_settings( self, settings ):
-        """ Returns a settings module regardless of module or dict or settings-path.
+        """ Returns a settings module regardless whether settings are passed in as a module or dict or settings-path.
             Called by BorrowDirect.__init__() """
         types = [ NoneType, dict, ModuleType, unicode ]
         assert type(settings) in types, Exception( u'Passing in settings is optional, but if used, must be either a dict, a unicode path to a settings module, or a module named settings; current type is: %s' % repr(type(settings)) )
