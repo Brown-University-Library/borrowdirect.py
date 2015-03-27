39eed0e59f7df2efe269657f3655da766268b060
birkin <birkin.diana@gmail.com>
2015-03-27 09:59:10 -0400

    adds & updates tests.

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@
 import exceptions, imp, pprint, os, time, unittest
 from bdpy import BorrowDirect
 from bdpy.auth import Authenticator
+from bdpy.search import Searcher
 
 
 SLEEP_SECONDS = 2  # test-server is creaky
@@ -52,7 +53,7 @@ class BorrowDirectTests( unittest.TestCase ):
         self.assertEqual(
             True, bd.authnz_valid )
 
-    def test_search(self):
+    def test_run_search(self):
         """ Tests item availability. """
         data = {
             u'UNIVERSITY_CODE': unicode( os.environ[u'BDPY_TEST__UNIVERSITY_CODE'] ),
@@ -69,25 +70,42 @@ class BorrowDirectTests( unittest.TestCase ):
                 key in result_dct[u'Item'].keys() )
         # NOTE: where is the 'RequestLink' key?
 
+    # def test_search(self):
+    #     """ Tests item availability. """
+    #     data = {
+    #         u'UNIVERSITY_CODE': unicode( os.environ[u'BDPY_TEST__UNIVERSITY_CODE'] ),
+    #         u'API_URL_ROOT': unicode( os.environ[u'BDPY_TEST__API_URL_ROOT'] ),
+    #         u'API_PARTNERSHIP_ID': unicode( os.environ[u'BDPY_TEST__PARTNERSHIP_ID'] ),
+    #         u'LOG_PATH': self.LOG_PATH }
+    #     bd = BorrowDirect( data )
+    #     patron_barcode = unicode(os.environ[u'BDPY_TEST__PATRON_BARCODE_GOOD'])
+    #     result_dct = bd.search( patron_barcode, u'ISBN', u'9780688002305' )  # Zen & the Art of Motorcycle Maintenance (also #0688002307)
+    #     # bd.search( patron_barcode, u'ISBN', u'9780307269706' )
+    #     # bd.search( patron_barcode, u'ISBN', u'0745649890' )
+    #     for key in [u'AuthorizationId', u'Available', u'PickupLocations', u'SearchTerm']:
+    #         self.assertTrue(
+    #             key in result_dct[u'Item'].keys() )
+    #     # NOTE: where is the 'RequestLink' key?
+
     # end class BorrowDirectTests
 
 
-class AuthTests( unittest.TestCase ):
+class AuthenticatorTests( unittest.TestCase ):
 
     def setUp(self):
         time.sleep( SLEEP_SECONDS )
         self.LOG_PATH = unicode( os.environ[u'BDPY_TEST__LOG_PATH'] )  # if None  ...outputs to console
         bd = BorrowDirect( {u'LOG_PATH': self.LOG_PATH} )
         self.logger = bd.logger
+        self.patron_barcode = unicode(os.environ[u'BDPY_TEST__PATRON_BARCODE_GOOD'])
+        self.api_url_root = unicode(os.environ[u'BDPY_TEST__API_URL_ROOT'])
+        self.university_code = unicode(os.environ[u'BDPY_TEST__UNIVERSITY_CODE'])
 
     def test_authenticate(self):
         """ Tests getting an authentication-id. """
-        ## good patron
         a = Authenticator( self.logger )
         authentication_id = a.authenticate(
-            unicode(os.environ[u'BDPY_TEST__PATRON_BARCODE_GOOD']),
-            unicode(os.environ[u'BDPY_TEST__API_URL_ROOT']),
-            unicode(os.environ[u'BDPY_TEST__UNIVERSITY_CODE']) )
+            self.patron_barcode, self.api_url_root, self.university_code )
         self.assertEqual(
             27, len(authentication_id) )
 
@@ -95,17 +113,40 @@ class AuthTests( unittest.TestCase ):
         """ Tests authz session-extender. """
         a = Authenticator( self.logger )
         authentication_id = a.authenticate(
-            unicode(os.environ[u'BDPY_TEST__PATRON_BARCODE_GOOD']),
-            unicode(os.environ[u'BDPY_TEST__API_URL_ROOT']),
-            unicode(os.environ[u'BDPY_TEST__UNIVERSITY_CODE']) )
+            self.patron_barcode, self.api_url_root, self.university_code )
         time.sleep( SLEEP_SECONDS )
         validity = a.authorize(
-            unicode(os.environ[u'BDPY_TEST__API_URL_ROOT']),
-            authentication_id )
+            self.api_url_root, authentication_id )
         self.assertEqual(
             True, validity )
 
     # end class AuthTests
+
+
+class SearcherTests( unittest.TestCase ):
+
+    def setUp(self):
+        time.sleep( SLEEP_SECONDS )
+        self.LOG_PATH = unicode( os.environ[u'BDPY_TEST__LOG_PATH'] )  # if None  ...outputs to console
+        bd = BorrowDirect( {u'LOG_PATH': self.LOG_PATH} )
+        self.logger = bd.logger
+        self.patron_barcode = unicode(os.environ[u'BDPY_TEST__PATRON_BARCODE_GOOD'])
+        self.api_url_root = unicode(os.environ[u'BDPY_TEST__API_URL_ROOT'])
+        self.university_code = unicode(os.environ[u'BDPY_TEST__UNIVERSITY_CODE'])
+        self.api_partnership_id = unicode(os.environ[u'BDPY_TEST__PARTNERSHIP_ID'])
+
+    def test_search(self):
+        """ Tests basic key-value search. """
+        s = Searcher( self.logger )
+        ( search_key, search_value ) = ( u'ISBN', u'9780688002305' )  # Zen & the Art of Motorcycle Maintenance (also #0688002307)
+        result_dct = s.search(
+            self.patron_barcode, search_key, search_value, self.api_url_root, self.university_code, self.api_partnership_id )
+        for key in [u'AuthorizationId', u'Available', u'PickupLocations', u'SearchTerm']:
+            self.assertTrue(
+                key in result_dct[u'Item'].keys() )
+        # NOTE: where is the 'RequestLink' key?
+
+    # end class SearcherTests
 
 
 
